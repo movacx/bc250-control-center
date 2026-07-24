d6c1bc644104197b54539c420fc74177c2f4ef90
Fabian Beita <fabianbeita36@gmail.com>
2026-07-24 06:49:05 -0600

    General program correction and optimization

--- mvc/Model/rendimiento.py
+++
@@ -1,9 +0,0 @@
-class Rendimiento:
-    def __init__(self, datos):
-        self.datos = datos
-
-    def get(self, clave, defecto=None):
-        return self.datos.get(clave, defecto)
-
-    def to_dict(self):
-        return self.datos
